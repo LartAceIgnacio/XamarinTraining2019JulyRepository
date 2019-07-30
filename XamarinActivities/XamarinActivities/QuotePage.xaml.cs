f0ad8f61919c54e05e24cae31f942eb7c17893da
mmejidana <mmejidana@BAIDEVLP103>
2019-07-30 16:49:46 +0800

    Add project files.

---
+++ XamarinActivities/XamarinActivities/QuotePage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinActivities
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class QuotePage : ContentPage
+    {
+        private int count;
+        private List<string> quoteList;
+        public QuotePage()
+        {
+            InitializeComponent();
+            slider.Value = 10;
+            slider.Maximum = 30;
+            slider.Minimum = 0;
+
+            quoteList = new List<String>()
+            {
+               "Don't cry because it's over, smile because it happened.",
+               "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind",
+               "A room without books is like a body without a soul.",
+               "You only live once, but if you do it right, once is enough",
+               "Be the change that you wish to see in the world.",
+               "In three words I can sum up everything I've learned about life: it goes on.",
+               "If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals.",
+               "No one can make you feel inferior without your consent.",
+               "If you tell the truth, you don't have to remember anything.",
+               "Be yourself; everyone else is already taken.",
+               "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe."
+            };
+
+            count = 0;
+            quote.Text = quoteList[count];
+        }
+
+        private void OnPreviousPageButtonClicked(object sender, EventArgs e)
+        {
+            if (count == 0)
+            {
+                quote.Text = quoteList[count];
+            } else
+            {
+                quote.Text = quoteList[count-1];
+                count -= 1;
+            }
+        }
+
+        private void OnNextPageButtonClicked(object sender, EventArgs e)
+        {
+
+            if (count == quoteList.Count-1) //last index
+            {
+                quote.Text = quoteList[count];
+            }
+            else
+            {
+                quote.Text = quoteList[count + 1];
+                count += 1;
+            }
+        }
+    }
+}
---
+++ XamarinActivities/XamarinActivities/RGBPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinActivities
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RGBPage : ContentPage
+    {
+        public RGBPage()
+        {
+            InitializeComponent();
+
+            var max = 255;
+            var min = 0;
+
+            //rgb_result = new BoxView { BackgroundColor = Color.FromRgb(red_slider.Value, green_slider.Value, blue_slider.Value) };
+            //result.Color = Color.FromRgb(0, 0, 0);
+            result.Color = Color.FromRgb(red_slider.Value, green_slider.Value, blue_slider.Value);
+
+            red_slider.Maximum = max;
+            blue_slider.Maximum = max;
+            green_slider.Maximum = max;
+
+            red_slider.Minimum = min;
+            blue_slider.Minimum = min;
+            green_slider.Minimum = min;
+
+            red_slider.Value = 255;
+            blue_slider.Value = 0;
+            green_slider.Value = 35;
+
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            result.Color = Color.FromRgb(red_slider.Value, green_slider.Value, blue_slider.Value);
+        }
+    }
+}
