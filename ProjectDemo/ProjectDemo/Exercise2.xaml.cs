cb072ca62f60bbf7d2bf4108e181311cf15ad6e5
BlastAsia Cloud02 <baicloud02@outlook.com>
2019-07-31 09:47:52 +0800

    Add project files.

---
+++ ProjectDemo/ProjectDemo/Exercise2.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProjectDemo
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page1 : ContentPage
+	{
+		public Page1()
+		{
+			InitializeComponent();
+			colorbox.Color = Color.FromRgb(0, 0, 0);
+			label1.Text = "Red : 0";
+			label2.Text = "Gree : 0";
+			label3.Text = "Blue : 0";
+		}
+
+		private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+		{
+			colorbox.Color = Color.FromRgb((int)slider1.Value,
+										  (int)slider2.Value,
+										  (int)slider3.Value);
+		}
+
+	}
+}
---
+++ ProjectDemo/ProjectDemo/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ProjectDemo
+{
+	// Learn more about making custom code visible in the Xamarin.Forms previewer
+	// by visiting https://aka.ms/xamarinforms-previewer
+	[DesignTimeVisible(false)]
+	public partial class MainPage : ContentPage
+	{
+		public string[] quotes = {"Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+			"The most disastrous thing that you can ever learn is your first programming language",
+			"Code is like humor. When you have to explain it, it’s bad.",
+			"Before software can be reusable it first has to be usable.",
+			"One of the best programming skills you can have is knowing when to walk away for awhile."
+		};
+		public MainPage()
+		{
+			InitializeComponent();
+			slider.Value = 15;
+			quote.Text = quotes[0];
+		}
+
+		private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+		{
+			fontSize.Text = String.Format("Font Size: {0:F0}", e.NewValue);
+		}
+		int count = 0;
+		private void Next_Quote(object sender, EventArgs e)
+		{
+			if(count==4)
+			{
+				count=0;
+				quote.Text = quotes[count];
+			}
+			else
+			{
+				count++;
+				quote.Text = quotes[count];
+			}
+			
+		}
+		private void Previous_Quote(object sender, EventArgs e)
+		{
+			if (count == 0)
+			{
+				count = 4;
+				quote.Text = quotes[count];
+			}
+			else
+			{
+				count--;
+				quote.Text = quotes[count];
+			}
+
+		}
+	}
+}
