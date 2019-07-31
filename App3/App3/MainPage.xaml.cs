5c94adb78fb7a723e81603bfa5f73a132979e67e
felixalexandercarao <drlexan211@yahoo.com>
2019-07-31 08:04:54 +0800

    Add project files.

---
+++ App3/App3/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App3
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            redLabel.Text = String.Format("Red : {0:0}", RedSlider.Value * 255);
+            greenLabel.Text = String.Format("Green : {0:0}", GreenSlider.Value * 255);
+            blueLabel.Text = String.Format("Green : {0:0}", BlueSlider.Value * 255);
+        }
+        void Handle_ValueChanged(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            Box.Color = Color.FromRgb(RedSlider.Value, GreenSlider.Value, BlueSlider.Value);
+            redLabel.Text = String.Format("Red : {0:0}", RedSlider.Value * 255);
+            greenLabel.Text = String.Format("Green : {0:0}", GreenSlider.Value * 255);
+            blueLabel.Text = String.Format("Green : {0:0}", BlueSlider.Value * 255);
+        }
+    }
+}
---
+++ App3/App3/Quotes.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Quotes : ContentPage
+    {
+        public Quotes()
+        {
+            InitializeComponent();
+            DisplayedQuote.Text = _quotes[quoteNumber];
+            FontSlider.Value = 16;
+            FontSize.Text = String.Format("Font Size : {0:0}", FontSlider.Value);
+            DisplayedQuote.FontSize = FontSlider.Value;
+        }
+        String[] _quotes = new string[]
+        {
+            "1.Suffering is a brand of a warrior",
+            "2.Few men greater then me",
+            "3.You Ought to look farther ahead",
+            "4.Chaos is a ladder",
+            "5.SThe things we do for love"
+        };
+        int quoteNumber = 0;
+        private void Back_Clicked(object sender, EventArgs e)
+        {
+            if (quoteNumber == 0)
+            {
+                quoteNumber = _quotes.Length-1;
+            }
+            else
+            {
+                quoteNumber -= 1;
+            }
+            DisplayedQuote.Text = _quotes[quoteNumber];
+        }
+
+        private void Next_Clicked(object sender, EventArgs e)
+        {
+            if (quoteNumber == _quotes.Length-1)
+            {
+                quoteNumber =0;
+            }
+            else
+            {
+                quoteNumber += 1;
+            }
+            DisplayedQuote.Text = _quotes[quoteNumber];
+        }
+
+        private void FontSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            FontSize.Text= String.Format("Font Size : {0:0}", FontSlider.Value);
+            DisplayedQuote.FontSize = FontSlider.Value;
+        }
+    }
+}
