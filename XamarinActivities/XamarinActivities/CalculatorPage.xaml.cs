5c0d46a9ef683f841a0b0742d11c314ef9aa3e1a
Kyla C <kyla.calpito@gmail.com>
2019-08-01 12:31:49 +0800

    Add functions for simple operations

--- XamarinActivities/XamarinActivities/CalculatorPage.xaml.cs
+++ XamarinActivities/XamarinActivities/CalculatorPage.xaml.cs
@@ -12,6 +12,10 @@ namespace XamarinActivities
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CalculatorPage : ContentPage
     {
+        String num1 = String.Empty;
+        String num2 = String.Empty;
+        char operation;
+        Double result = 0.00;
 
         public CalculatorPage()
         {
@@ -20,21 +24,51 @@ namespace XamarinActivities
 
         private void Number_Button_Clicked(object sender, EventArgs e)
         {
-            String numberFormat = "#,##0";
             var button = sender as Button;
-
             var number = Number_Label.Text + button.Text;
-            Number_Label.Text = Convert.ToDecimal(number).ToString(numberFormat);
+            Number_Label.Text = String.Format("{0:n0}", Double.Parse(number)); 
         }
 
         private void Operator_Button_Clicked(object sender, EventArgs e)
         {
+            num1 = Number_Label.Text;
+            Number_Label.Text = String.Empty;
             var button = sender as Button;
+            operation = Char.Parse(button.Text);
+        }
+
+        private void Equal_Button_Clicked(object sender, EventArgs e)
+        {
+            var operand1 = Double.Parse(num1);
+            var operand2 = Double.Parse(Number_Label.Text);
+
+            if (operation == '+')
+            {
+                result = operand1 + operand2;
+            }
+            else if (operation == '-')
+            {
+                result = operand1 - operand2;
+            }
+            else if (operation == '×')
+            {
+                result = operand1 * operand2;
+            }
+            else if (operation == '÷')
+            {
+                result = operand1 / operand2;
+            }
+            else if (operation == '%')
+            {
+                result = operand1 % operand2;
+            }
+
+            Number_Label.Text = String.Format("{0:n0}", result);
         }
 
         private void Clear_Button_Clicked(object sender, EventArgs e)
         {
-            Number_Label.Text = "";
+            Number_Label.Text = String.Empty;
         }
     }
 }
