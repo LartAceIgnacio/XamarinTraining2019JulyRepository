967a3ca4644f75d26b5df677585464c637da3548
BlastAsia Cloud02 <baicloud02@outlook.com>
2019-08-05 09:43:34 +0800

    Image First Exercise Created, not finish

---
+++ ProjectDemo/ProjectDemo/ImageExercise1Page.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProjectDemo
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ImageExercise1Page : ContentPage
+	{
+		public ImageExercise1Page()
+		{
+			InitializeComponent();
+		}
+	}
+}
