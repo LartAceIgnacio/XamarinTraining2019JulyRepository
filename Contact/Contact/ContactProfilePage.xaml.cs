5d3a9cab0dd0bc0da9f20e3a21c7fe768b7ad52e
nazarenocharleskenichi@gmail.com <nazarenocharleskenichi@gmail.com>
2019-08-07 16:56:31 +0800

    Add project files.

---
+++ Contact/Contact/ContactProfilePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Contact
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ContactProfilePage : ContentPage
+    {
+        public ContactProfilePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Contact/Contact/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Contact
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        async void ContactListPage_button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ContactListPage());
+        }
+    }
+}
