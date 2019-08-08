b23dd1467ce81a544ecf8db38d5494c9e4a4a35e
orilla-jasper <jasper.orilla915@gmail.com>
2019-08-08 15:46:50 +0800

    add database

---
+++ XamarinExercise/XamarinExercise.Android/ContactsDb_Droid.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+using XamarinExercise.Database;
+using XamarinExercise.Droid;
+using XamarinExercise.Models;
+
+[assembly: Xamarin.Forms.Dependency(typeof(ContactsDb_Droid))]
+namespace XamarinExercise.Droid
+{
+    public class ContactsDb_Droid : ISQLite
+    {
+        public SQLiteConnection GetConnection()
+        {
+            var sqliteFileName = "ContactsDb.db3";
+            string documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal); //Documents folder
+            var path = Path.Combine(documentsPath, sqliteFileName);
+
+            var conn = new SQLiteConnection(path, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create | SQLiteOpenFlags.SharedCache);
+
+            return conn;
+        }
+    }
+}
---
+++ XamarinExercise/XamarinExercise/Database/ISQLite.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinExercise.Database
+{
+    public interface ISQLite
+    {
+        SQLiteConnection GetConnection();
+    }
+}
